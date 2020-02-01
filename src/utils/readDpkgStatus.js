8a0a95656a20709dadfa0eb4635d554d16559a01
Linus Willner <hello@linuswillner.me>
2020-02-01 23:21:39 +0200

    Add comment about os.platform() check

--- src/utils/readDpkgStatus.js
+++ src/utils/readDpkgStatus.js
@@ -10,6 +10,7 @@ module.exports = async () => {
     return readFile('/var/lib/dpkg/status', 'utf-8')
   } catch (err) {
     console.error('ERROR: Could not read /var/lib/dpkg/status:', err)
+    // If the user attempts to run this on Windows, they misunderstood something - if the file doesn't exist on Linux, something is seriously broken
     console.error(os.platform() === 'win32' ? 'This operating system does not appear to be a Linux system.' : 'Please check your operating system installation.')
   }
 }
