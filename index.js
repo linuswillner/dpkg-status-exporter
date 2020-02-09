e9fdfd4ce09625bf2199904664377aafe9ea5d2d
Linus Willner <hello@linuswillner.me>
2020-02-09 20:06:47 +0200

    Finalise exporter

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+const path = require('path')
 const debCtrlToJson = require('./src/debian-control-to-json')
 const readDpkgStatus = require('./src/utils/readDpkgStatus')
-const filterExtraFields = require('./src/formatters/filterExtraFields')
-const formatDepends = require('./src/formatters/formatDepends')
+const createHTMLIndex = require('./src/utils/createHTMLIndex')
+const {
+  filterExtraFields,
+  serializeDepends,
+  parseDependents,
+  generateHTML
+} = require('./src/formatters')
 
 ;(async () => {
-  const dpkgStatus = await readDpkgStatus() // Read /var/lib/dpkg/status
-  const serialised = await debCtrlToJson(dpkgStatus) // Serialize Debian control file syntax to JSON
-  const filtered = filterExtraFields(serialised) // Filter redundant fields from the serialised data
-  const formatted = formatDepends(filtered)
-  // TODO: Add reverse dependency logic
-  // TODO: Write HTML formatter next
+  try {
+    const dpkgStatus = await readDpkgStatus() // Read /var/lib/dpkg/status
+    const serialised = await debCtrlToJson(dpkgStatus) // Serialize Debian control file syntax to JSON
+    const filtered = filterExtraFields(serialised) // Filter redundant fields from the serialised data
+    const formatted = serializeDepends(filtered) // Serialize Depends field
+    const withDependents = await parseDependents(formatted) // Parse out the dependents of each package
+    const htmlList = generateHTML(withDependents) // Generate an HTML index of the package index
+    await createHTMLIndex(htmlList) // Write the final output to out/index.html
+    console.log(`Successfully exported /var/lib/dpkg/status to ${path.join(process.cwd(), 'out/index.html')}`)
+  } catch (err) {
+    console.error('Error: Could not export /var/lib/dpkg/status to HTML: ', err)
+  }
 })()
