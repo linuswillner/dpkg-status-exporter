28cecf6387fadc2eaedf4de18840e043d70063c1
Linus Willner <hello@linuswillner.me>
2020-02-09 20:05:36 +0200

    Add reverse dependency (Dependent) parser

---
+++ src/formatters/parseDependents.js
@@ -0,0 +1,29 @@
+/**
+ * Scan the package list for dependents of a pacakge
+ * @param {String} packageName The name of the package to scan for
+ * @param {Object} packageList Serialised dpkg status data that has dependencies already parsed
+ */
+const scanForDependents = (packageName, packageList) => {
+  const dependents = []
+
+  for (const pkg in packageList) {
+    // Dependencies are listed with their names as keys, format them into an array for easier inclusion checking
+    const dependencies = Object.keys(packageList[pkg].Depends)
+    if (dependencies.includes(packageName)) dependents.push(pkg)
+  }
+
+  return dependents
+}
+
+/**
+ * Map package dependents such that each package has a list of possible dependents
+ * @param {Object} packageList Serialised dpkg status data that has dependencies already parsed
+ */
+module.exports = async packageList => {
+  for (const pkg in packageList) {
+    const dependents = scanForDependents(pkg, packageList)
+    packageList[pkg].Dependents = dependents
+  }
+
+  return packageList
+}
