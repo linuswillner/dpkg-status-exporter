5147cf6dfa2b00feeff9440a581344f55fa8f47f
Linus Willner <hello@linuswillner.me>
2020-02-09 19:52:42 +0200

    Improve logging for readDpkgStatus

--- src/utils/readDpkgStatus.js
+++ src/utils/readDpkgStatus.js
@@ -9,8 +9,10 @@ module.exports = async () => {
   try {
     return readFile('/var/lib/dpkg/status', 'utf-8')
   } catch (err) {
-    console.error('ERROR: Could not read /var/lib/dpkg/status:', err)
-    // If the user attempts to run this on Windows, they misunderstood something - if the file doesn't exist on Linux, something is seriously broken
-    console.error(os.platform() === 'win32' ? 'This operating system does not appear to be a Linux system.' : 'Please check your operating system installation.')
+    console.error('Error: Could not read /var/lib/dpkg/status:', err)
+    console.error(os.platform() === 'win32'
+      ? 'This operating system does not appear to be a *nix system.'
+      : 'Your operating system may be using a package manager other than Aptitude, or the file may be missing/corrupted.'
+    )
   }
 }
