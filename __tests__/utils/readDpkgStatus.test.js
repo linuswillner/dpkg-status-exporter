d4683301f2bb735de8df3ebfb8e6d6588f1ff162
Linus Willner <hello@linuswillner.me>
2020-02-17 22:15:34 +0200

    Slightly re-organise mock calls in readDpkgStatus test suite

--- __tests__/utils/readDpkgStatus.test.js
+++ __tests__/utils/readDpkgStatus.test.js
@@ -8,16 +8,16 @@ const os = require('os')
 
 describe('readDpkgStatus utility', () => {
   it('throws an unsupported OS error on Windows', async () => {
-    fs.__setExistsSyncOverride(false)
     os.__setPlatformOverride('win32')
+    fs.__setExistsSyncOverride(false)
 
     await expect(readDpkgStatus('/var/lib/dpkg/status/')).rejects
       .toThrowError('This operating system does not appear to be a *nix system.')
   })
 
   it('throws an unsupported package manager or file corruption error on Linux', async () => {
-    fs.__setExistsSyncOverride(false)
     os.__setPlatformOverride('linux')
+    fs.__setExistsSyncOverride(false)
 
     await expect(readDpkgStatus('/var/lib/dpkg/status/')).rejects
       .toThrowError('The operating system may be using a package manager other than Aptitude, or the file may be missing/corrupted.')
