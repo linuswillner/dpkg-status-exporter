c3b9336804b1a0fd4013d4431ddabaebbd514895
Linus Willner <hello@linuswillner.me>
2020-02-22 17:17:21 +0200

    Slightly tweak not found error for the status index

--- src/utils/readDpkgStatus.js
+++ src/utils/readDpkgStatus.js
@@ -6,9 +6,11 @@ const readFile = util.promisify(fs.readFile)
 
 module.exports = async statusFile => {
   if (!fs.existsSync(statusFile)) {
+    let error = 'Could not find /var/lib/dpkg/status. '
+
     throw new Error(os.platform() === 'win32'
-      ? 'This operating system does not appear to be a *nix system.'
-      : 'The operating system may be using a package manager other than Aptitude, or the file may be missing/corrupted.'
+      ? error += 'This operating system does not appear to be a *nix system.'
+      : error += 'The operating system may be using a package manager other than Aptitude, or the file may be missing/corrupted.'
     )
   }
 
